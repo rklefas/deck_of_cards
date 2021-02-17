ca8ef11f168d953a54be0d0f0378992aaa21006e
Ryan Klefas <ryan.klefas@gmail.com>
2021-02-16 21:56:59 -0500

    Add composer integration and unit tests.

---
+++ tests/DeckTest.php
@@ -0,0 +1,54 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+final class DeckTest extends TestCase
+{
+    public function testFirstCardOfUnshuffledDeck()
+    {
+		$deck = new CardDeck;
+
+		$this->assertEquals($deck->deal_one_card(), "Ace of Clubs");
+    }
+	
+	
+	public function testNumberDealtIncreasesOnEachDeal()
+    {
+		$deck = new CardDeck;
+		
+		for ($loops = 1; $loops < 53; $loops++)
+		{
+			$deck->deal_one_card();
+
+			$this->assertEquals($deck->get_number_dealt(), $loops);
+		}
+		
+    }
+
+    public function testShufflingChangesDeck()
+    {
+		$deck = new CardDeck;
+		
+		while ($card = $deck->deal_one_card())
+		{
+			$unshuffled[] = $card;
+		}
+		
+		$deck = new CardDeck;
+		$deck->shuffle();
+
+		while ($card = $deck->deal_one_card())
+		{
+			$shuffled[] = $card;
+		}
+
+		$this->assertNotEquals($unshuffled, $shuffled);
+    }
+	
+
+
+}
+
+
+
+
+
---
+++ tests/FirstTest.php
@@ -0,0 +1,16 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+final class FirstTest extends TestCase
+{
+    public function testValueIsTrue()
+    {
+        $this->assertTrue( true );
+    }
+
+}
+
+
+
+
+
