d6393a043e3b1abf0c4fe64e74e8baea48195f5d
Ryan Klefas <ryan.klefas@gmail.com>
2021-02-18 11:18:35 -0500

    Add more comments.

--- src/class.shuffler.php
+++ src/class.shuffler.php
@@ -4,37 +4,35 @@
 
 class Shuffler
 {
-	
-	/**
+
+    /**
     * A reusable algorithm for shuffling an array.
     */
     static public function shuffle_algorithm($array, $shuffleRemaining = 10)
     {
-		if ($shuffleRemaining > 0)
-		{
-			// Break the array into two piles
-			$piles = [];
-			
-			foreach ($array as $item)
-			{
-				// Randomly decide which pile the card goes into
-				
-				$piles[ mt_rand(0, 1) ? 'one' : 'two' ][] = $item;
-			}
-			
-			// Put the piles back together
-			
-			$combined = array_merge($piles['one'], $piles['two']);
-			
-			// Shuffle the array again
-			
-			$combined = self::shuffle_algorithm($combined, $shuffleRemaining - 1);
-			
-			return $combined;
-		}
-		
-		return $array;
+        if ($shuffleRemaining > 0)
+        {
+            // Break the array into two piles
+            $piles = [];
+
+            foreach ($array as $item)
+            {
+                // Randomly decide which pile the card goes into
+                $piles[ mt_rand(0, 1) ? 'one' : 'two' ][] = $item;
+            }
+
+            // Put the piles back together
+            $combined = array_merge($piles['one'], $piles['two']);
+
+            // Shuffle the array again
+            $combined = self::shuffle_algorithm($combined, $shuffleRemaining - 1);
+
+            return $combined;
+        }
+
+        // Just return the array if we have shuffled it enough
+        return $array;
     }
-	
+
 
 }
