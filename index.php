ae1eb20b2fd45d1c9bb3c0caf2f9d376286eb1d8
Ryan Klefas <ryan.klefas@gmail.com>
2021-02-18 09:52:11 -0500

    Use twig template.

--- index.php
+++ index.php
@@ -4,14 +4,21 @@
 require "vendor/autoload.php";
 
 
+
+
+
 $game = new CardDeck;
 $game->shuffle();
 
 while ($card = $game->deal_one_card())
 {
-	echo "Card #{$game->get_number_dealt()} is {$card}\n";
+	$display[] = "Card #{$game->get_number_dealt()} is {$card}\n";
 }
 
 
-// call #53 is null
-var_dump($game->deal_one_card());
+$loader = new \Twig\Loader\FilesystemLoader('./templates/');
+$twig = new \Twig\Environment($loader);
+
+$template = $twig->load('index.html');
+echo $template->render(['display' => $display]);
+
