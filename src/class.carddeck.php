be57345de2f51266b51f51b7d9d4f501beef7b50
Ryan Klefas <ryan.klefas@gmail.com>
2021-02-18 10:44:40 -0500

    Fix indents.

--- src/class.carddeck.php
+++ src/class.carddeck.php
@@ -3,58 +3,65 @@
 
 class CardDeck
 {
-	private $deck = [];
-	private $dealt = [];
-	
-	public function __construct()
-	{
-		$ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
-		$suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
-		
-		foreach ($suits as $suit)
-		{
-			foreach ($ranks as $rank)
-			{
-				$this->deck[] = $rank." of ".$suit;
-			}
-		}
-	}
-	
-	
-	private function shuffle_algorithm($array)
-	{
-		$newArray = [];
-		
-		foreach ($array as $card)
-		{
-			$newArray[$card] = mt_rand(1, 1000000);
-		}
-		
-		asort($newArray);
-		
-		return array_keys($newArray);
-	}
-	
-	
-	
-	public function shuffle()
-	{
-		$this->deck = $this->shuffle_algorithm($this->deck);
-	}
-	
-	
-	
-	public function deal_one_card()
-	{
-		$popped = array_pop($this->deck);
-		$this->dealt[] = $popped;
-		return $popped;
-	}
-	
-	
-	public function get_number_dealt()
-	{
-		return count($this->dealt);
-	}
-	
+    private $deck = [];
+    private $dealt = [];
+
+    public function __construct()
+    {
+        $ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
+        $suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+
+        foreach ($suits as $suit)
+        {
+            foreach ($ranks as $rank)
+            {
+                $this->deck[] = $rank." of ".$suit;
+            }
+        }
+    }
+
+    /**
+    * A reusable algorithm for shuffling an array.
+    */
+    private function shuffle_algorithm($array)
+    {
+        $newArray = [];
+
+        foreach ($array as $card)
+        {
+            $newArray[$card] = mt_rand(1, 1000000);
+        }
+
+        asort($newArray);
+
+        return array_keys($newArray);
+    }
+
+    /**
+    * Shuffle the deck
+    */
+    public function shuffle()
+    {
+        $this->deck = $this->shuffle_algorithm($this->deck);
+    }
+
+
+    /**
+    * Pop the last card out of the deck and return it.
+    */
+    public function deal_one_card()
+    {
+        $popped = array_pop($this->deck);
+        $this->dealt[] = $popped;
+        return $popped;
+    }
+
+    /**
+    * Return a count of the number of cards already dealt.
+    */
+    public function get_number_dealt()
+    {
+        return count($this->dealt);
+    }
+
 }
