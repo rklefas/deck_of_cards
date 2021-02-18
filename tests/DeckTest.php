cb2949e24989043fe56848137440a098ccf2c950
Ryan Klefas <ryan.klefas@gmail.com>
2021-02-18 11:25:53 -0500

    Add more assertions to the shuffle test.

--- tests/DeckTest.php
+++ tests/DeckTest.php
@@ -3,46 +3,49 @@ use PHPUnit\Framework\TestCase;
 
 final class DeckTest extends TestCase
 {
-  public function testFirstCardOfUnshuffledDeck()
-  {
-    $deck = new CardDeck;
-
-    $this->assertEquals($deck->deal_one_card(), "Ace of Clubs");
-  }
-
-
-  public function testNumberDealtIncreasesOnEachDeal()
-  {
-    $deck = new CardDeck;
-
-    for ($loops = 1; $loops < 53; $loops++)
+    public function testFirstCardOfUnshuffledDeck()
     {
-      $deck->deal_one_card();
+        $deck = new CardDeck;
 
-      $this->assertEquals($deck->get_number_dealt(), $loops);
+        $this->assertEquals($deck->deal_one_card(), "Ace of Clubs");
     }
 
-  }
 
-  public function testShufflingChangesDeck()
-  {
-    $deck = new CardDeck;
-
-    while ($card = $deck->deal_one_card())
+    public function testNumberDealtIncreasesOnEachDeal()
     {
-      $unshuffled[] = $card;
+        $deck = new CardDeck;
+
+        for ($loops = 1; $loops < 53; $loops++)
+        {
+            $deck->deal_one_card();
+
+            $this->assertEquals($deck->get_number_dealt(), $loops);
+        }
+
     }
 
-    $deck = new CardDeck;
-    $deck->shuffle();
-
-    while ($card = $deck->deal_one_card())
+    public function testShufflingChangesDeck()
     {
-      $shuffled[] = $card;
-    }
+        $deck = new CardDeck;
 
-    $this->assertNotEquals($unshuffled, $shuffled);
-  }
+        while ($card = $deck->deal_one_card())
+        {
+            $unshuffled[] = $card;
+        }
+
+        $deck = new CardDeck;
+        $deck->shuffle();
+
+        while ($card = $deck->deal_one_card())
+        {
+            $shuffled[] = $card;
+        }
+
+        $this->assertEquals(count($unshuffled), 52);
+        $this->assertEquals(count($shuffled), 52);
+
+        $this->assertNotEquals($unshuffled, $shuffled);
+    }
 
 
 
